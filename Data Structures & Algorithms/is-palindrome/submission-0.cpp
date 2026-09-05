a1bff12c63f99a85fa2839b237d06af1d9ff3504
SamarthMehta <85919984+SamarthMehta@users.noreply.github.com>
2026-09-06 00:13:38 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newStr = "";
+        for(char c : s){
+            if(isalnum(c)){
+                newStr += tolower(c);
+            }
+        }
+
+        string newStrRev = newStr;
+        reverse(newStrRev.begin(),newStrRev.end());
+        return newStr == newStrRev;
+    }
+};
